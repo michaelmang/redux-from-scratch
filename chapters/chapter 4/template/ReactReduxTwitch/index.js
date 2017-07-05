286ca07482a68cacdd8b9c272842a6fc3e145dbd
Michael Mangialardi <mikemangialardi94@gmail.com>
2017-07-04 21:51:37 -0400

    Update index.js

---
+++ chapters/chapter 4/template/ReactReduxTwitch/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//top level of React component hierarchy
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <React + Redux + Twitch = 😍/>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App/>
+  document.getElementById('app')
+)
