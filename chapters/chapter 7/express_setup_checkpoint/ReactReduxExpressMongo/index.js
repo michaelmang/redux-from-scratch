e7c4f4fbf221456d299619432d23200ac17bddc4
Michael Mangialardi <mikemangialardi94@gmail.com>
2017-07-08 14:56:46 -0400

    Express Setup Checkpoint Code

---
+++ chapters/chapter 7/express_setup_checkpoint/ReactReduxExpressMongo/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import ApiApp from './reducers/ApiApp';
+
+import Streams from './components/containers/Streams';
+
+//top level of React component hierarchy
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <Streams store={store} />
+      </div>
+    )
+  }
+}
+
+//intialize store
+let store = createStore(
+  ApiApp,
+  applyMiddleware( thunk, logger )
+);
+
+ReactDOM.render(
+  <Provider store = { store }>
+    <App/>
+  </Provider>,
+  document.getElementById('app')
+)
---
+++ chapters/chapter 7/express_setup_checkpoint/ReactReduxExpressMongo/reducers/ApiApp.js
@@ -0,0 +1,32 @@
+//define the initial state
+const initialState = {
+  status: "loading",
+  streams: [0,1,2],
+  error: ""
+}
+
+//define a reducer with an intitalized state and logic to handle action
+function ApiApp(state = initialState, action) {
+  switch(action.type) {
+    case 'FETCH_REQUEST':
+      const requested = Object.assign({}, state, {
+        status: action.status
+      })
+      return requested
+    case 'FETCH_SUCCESS':
+      const successful = Object.assign({}, state, {
+        status: action.status
+      })
+      return successful
+    case 'FETCH_FAILURE':
+      const failed = Object.assign({}, state, {
+        status: action.status,
+        error: action.error
+      })
+      return failed
+    default:
+      return state
+  }
+}
+
+export default ApiApp
